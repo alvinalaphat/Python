d1337c06082c1ac871ac8a253cebef7f69546668
Alvin Alaphat <33328305+alvinalaphat@users.noreply.github.com>
2018-08-02 09:44:04 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,111 +1,221 @@
-from __future__ import print_function 
+from __future__ import print_function
+
 from IPython.display import clear_output
 
-answer = raw_input('do you want to play? ')
+
+
+answer = input('do you want to play? ')
+
 if answer == 'yes':
+
     print("OK let's play")
+
 else:
+
     print('Well ok, see ya!')
+
     exit()
-	
-board = [' '] * 10 
-   
+
+
+
+board = [' '] * 10
+
+
+
 def display_board(board):
-    
+
+
+
     clear_output()
+
     print('   |   |')
+
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+
     print('   |   |')
+
     print('-----------')
+
     print('   |   |')
+
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+
     print('   |   |')
+
     print('-----------')
+
     print('   |   |')
+
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+
     print('   |   |')
- 
+
+
+
 display_board(board)
+
 answer = []
 
+
+
 while (answer != 'X' and answer != 'O'):
-    answer = raw_input('Player 1, do you want to be X or O? ').upper()
+
+    answer = input('Player 1, do you want to be X or O? ').upper()
+
     continue
+
 if answer == 'O':
+
     print("OK then Player 2 will be X")
+
     p1 = 'O'
+
     p2 = 'X'
+
 else:
+
     print("OK then Player 2 will be O")
+
     p1 = 'X'
+
     p2 = 'O'
+
 answer = clear_output
 
-num1 = input('Player 1, make your move: ')
+
+
+num1 = int(input('Player 1, make your move: '))
+
 board[num1] = p1
+
 display_board(board)
 
-num2 = input('Player 2, make your move: ')
+
+
+num2 = int(input('Player 2, make your move: '))
+
 board[num2] = p2
+
 display_board(board)
 
-num1 = input('Player 1, make your move: ')
+
+
+num1 = int(input('Player 1, make your move: '))
+
 board[num1] = p1
+
 display_board(board)
 
-num2 = input('Player 2, make your move: ')
+
+
+num2 = int(input('Player 2, make your move: '))
+
 board[num2] = p2
+
 display_board(board)
 
-num1 = input('Player 1, make your move: ')
+
+
+num1 = int(input('Player 1, make your move: '))
+
 board[num1] = p1
+
 display_board(board)
+
 if p1 == board[1] == board[2] == board[3] or p1 == board[4] == board[5] == board[6] or p1 == board[7] == board[8] == board[9] or p1 == board[1] == board[5] == board[9] or p1 == board[3] == board[5] == board[7] or p1 == board[1] == board[4] == board[7] or p1 == board[2] == board[5] == board[8] or p1 == board[3] == board[6] == board[9]:
+
 	print('Player 1 wins!')
-	exit()
-if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
-	print('Player 2 wins!')
+
 	exit()
 
-num2 = input('Player 2, make your move: ')
+if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
+
+	print('Player 2 wins!')
+
+	exit()
+
+
+
+num2 = int(input('Player 2, make your move: '))
+
 board[num2] = p2
+
 display_board(board)
+
 if p1 == board[1] == board[2] == board[3] or p1 == board[4] == board[5] == board[6] or p1 == board[7] == board[8] == board[9] or p1 == board[1] == board[5] == board[9] or p1 == board[3] == board[5] == board[7] or p1 == board[1] == board[4] == board[7] or p1 == board[2] == board[5] == board[8] or p1 == board[3] == board[6] == board[9]:
+
 	print('Player 1 wins!')
-	exit()
-if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
-	print('Player 2 wins!')
+
 	exit()
 
-num1 = input('Player 1, make your move: ')
+if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
+
+	print('Player 2 wins!')
+
+	exit()
+
+
+
+num1 = int(input('Player 1, make your move: '))
+
 board[num1] = p1
+
 display_board(board)
+
 if p1 == board[1] == board[2] == board[3] or p1 == board[4] == board[5] == board[6] or p1 == board[7] == board[8] == board[9] or p1 == board[1] == board[5] == board[9] or p1 == board[3] == board[5] == board[7] or p1 == board[1] == board[4] == board[7] or p1 == board[2] == board[5] == board[8] or p1 == board[3] == board[6] == board[9]:
+
 	print('Player 1 wins!')
-	exit()
-if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
-	print('Player 2 wins!')
+
 	exit()
 
-num2 = input('Player 2, make your move: ')
+if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
+
+	print('Player 2 wins!')
+
+	exit()
+
+
+
+num2 = int(input('Player 2, make your move: '))
+
 board[num2] = p2
+
 display_board(board)
+
 if p1 == board[1] == board[2] == board[3] or p1 == board[4] == board[5] == board[6] or p1 == board[7] == board[8] == board[9] or p1 == board[1] == board[5] == board[9] or p1 == board[3] == board[5] == board[7] or p1 == board[1] == board[4] == board[7] or p1 == board[2] == board[5] == board[8] or p1 == board[3] == board[6] == board[9]:
+
 	print('Player 1 wins!')
-	exit()
-if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
-	print('Player 2 wins!')
+
 	exit()
 
-num1 = input('Player 1, make your move: ')
-board[num1] = p1
-display_board(board)
-if p1 == board[1] == board[2] == board[3] or p1 == board[4] == board[5] == board[6] or p1 == board[7] == board[8] == board[9] or p1 == board[1] == board[5] == board[9] or p1 == board[3] == board[5] == board[7] or p1 == board[1] == board[4] == board[7] or p1 == board[2] == board[5] == board[8] or p1 == board[3] == board[6] == board[9]:
-	print('Player 1 wins!')
-	exit()
 if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
+
 	print('Player 2 wins!')
+
 	exit()
+
+
+
+num1 = int(input('Player 1, make your move: '))
+
+board[num1] = p1
+
+display_board(board)
+
+if p1 == board[1] == board[2] == board[3] or p1 == board[4] == board[5] == board[6] or p1 == board[7] == board[8] == board[9] or p1 == board[1] == board[5] == board[9] or p1 == board[3] == board[5] == board[7] or p1 == board[1] == board[4] == board[7] or p1 == board[2] == board[5] == board[8] or p1 == board[3] == board[6] == board[9]:
+
+	print('Player 1 wins!')
+
+	exit()
+
+if p2 == board[1] == board[2] == board[3] or p2 == board[4] == board[5] == board[6] or p2 == board[7] == board[8] == board[9] or p2 == board[1] == board[5] == board[9] or p2 == board[3] == board[5] == board[7] or p2 == board[1] == board[4] == board[7] or p2 == board[2] == board[5] == board[8] or p2 == board[3] == board[6] == board[9]:
+
+	print('Player 2 wins!')
+
+	exit()
+
 else:
-	print("It's a draw!") 
+
+	print("It's a draw!")
+
 	exit()
